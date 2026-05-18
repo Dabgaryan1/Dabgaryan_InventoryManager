ab59c979c10373efd4f1f65ba8433d486a8e39de
Daniel Abgaryan <Dabgaryan04@gmail.com>
2026-05-18 15:56:34 -0700

    Initial commit

---
+++ src/main/java/com/daniel/inventorymanager/model/Item.java
@@ -0,0 +1,64 @@
+package com.daniel.inventorymanager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //data fields
+    private Long id;
+    private String name;
+    private int quantity;
+    private String category;
+
+    //default constructor
+    public Item() {
+    }
+
+    //parameterized constructor
+    public Item(String name, int quantity, String category) {
+        this.name = name;
+        this.quantity = quantity;
+        this.category = category;
+    }
+
+    //ID getter and setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    //name getter and setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //quantity getter and setter
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    //category getter and setter
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
---
+++ src/main/java/com/daniel/inventorymanager/repository/ItemRepository.java
@@ -0,0 +1,13 @@
+package com.daniel.inventorymanager.repository;
+
+import com.daniel.inventorymanager.model.Item;
+import java.util.List;
+import java.util.Optional;
+
+
+public interface ItemRepository extends org.springframework.data.jpa.repository.JpaRepository<com.daniel.inventorymanager.model.Item, Long> {
+    List<Item> findByQuantityLessThan(int quantity);
+    Optional<Item> findByName(String name);
+    List<Item> findByCategory(String category);
+    //jpa repository provides basic CRUD operations, so we don't need to define them here
+}
